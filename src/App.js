49ff90887aacefbcdc1022b3945c33f2fe34dea4
Bobby <Bobby@Bobbys-Creation-Station.local>
2019-01-05 00:12:44 -0700

    Created containers for content and updated styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Toolbar from './components/Toolbar/Toolbar';
 import SideDrawer from './components/SideDrawer/SideDrawer';
 import Backdrop from './components/Backdrop/Backdrop';
+import code from './code.mp4';
 
 class App extends Component {
   state = {
@@ -32,10 +33,21 @@ class App extends Component {
         <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
         <SideDrawer show={this.state.sideDrawerOpen} />
         {backdrop}
-        <div className = "content">
-        <p> This is the page Content!</p>
+        <div className = "content" id="about_me">
+        <h1> About Me Goes Here</h1>
+        </div>
+        <div className="work" id ="work">
+        <h1>Work Goes Here</h1>
+      </div>
+        <div className="Contact" id ="contact">
+        <video className='videoTag' autoPlay loop muted>
+          <source src={code} type='video/mp4' />
+        </video>
         </div>
       </div>
+
+  
+
     );
   }
 }
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -4,19 +4,20 @@ import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 import './Toolbar.css';
 import logo from './logo.png';
 
+
 const toolbar = props => (
 <header className="toolbar">
     <nav className="toolbar__navigation">
         <div className="toolbar__toggle-button">
             <DrawerToggleButton click={props.drawerClickHandler} />
         </div>
-        <div className="toolbar__logo"><a href="/"><img alt="logo" src={logo}></img></a></div>
+        <div className="toolbar__logo"><img alt="logo" src={logo}></img></div>
             <div className="spacer"></div>
             <div className="toolbar_navigation_items">
                 <ul>
-                    <li><a href="/">About Me</a></li>
-                    <li><a href="/">Work</a></li>
-                    <li><a href="/">Contact</a></li>
+                    <li><a href="#about_me">About Me</a></li>
+                    <li><a href="#work">Work</a></li>
+                    <li><a href="#contact">Contact</a></li>
                  </ul>
             </div>
         </nav>
