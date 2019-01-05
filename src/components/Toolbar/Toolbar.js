65d4f6e7fad3c73f2c452abfdb6898c9e19b69aa
Bobby <Bobby@Bobbys-Creation-Station.local>
2019-01-04 21:12:30 -0700

    Added hamburger menu

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 import './Toolbar.css';
 
 const toolbar = props => (
 <header className="toolbar">
     <nav className="toolbar__navigation">
-        <div></div>
+        <div>
+            <DrawerToggleButton />
+        </div>
         <div className="toolbar__logo"><a href="/">"THE LOGO"</a></div>
+            <div className="spacer"></div>
             <div className="toolbar_navigation_items">
                 <ul>
                     <li><a href="/">Products</a></li>
