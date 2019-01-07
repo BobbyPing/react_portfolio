708d6611ea6f0af42a348a0140f02259dd687793
Bobby <Bobby@Bobbys-Creation-Station.local>
2019-01-06 17:26:05 -0700

    Updated About Me Section and Started Media Queries

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Toolbar from './components/Toolbar/Toolbar';
 import SideDrawer from './components/SideDrawer/SideDrawer';
 import Backdrop from './components/Backdrop/Backdrop';
+import bp from './bp.JPG';
 
 class App extends Component {
   state = {
@@ -33,8 +34,29 @@ class App extends Component {
         <SideDrawer show={this.state.sideDrawerOpen} />
         {backdrop}
         <div className = "content" id="about_me">
-        <h1> About Me Goes Here</h1>
-        </div>
+          <div className = "portrait">
+            <img alt="bp" src ={bp}></img>
+          </div>
+          <h1> About Me </h1>
+             <h2>Hello! My name is Bobby Ping.  I am a Front End/React Developer and UI/UX Designer/Developer</h2>
+          <div className = "bio">
+            <span> 
+              <p>I am newer to Southern Utah and absolutely love it here! I love the outdoors and there is so much to see and do in this area. I am a father of two amazing children. We love to travel, hike, cook and try new foods, and play music. I also love art, arcitecture, and photography.After working for many years as an <b>IT Professional</b>, I <b>developed</b> a passion for <b>user experience</b>, <b>user interface</b>, and <b>development</b> in general.
+                I decided to get serious about <b>web development</b> and enrolled and graduated from Bottega Tech's Full Stack Certification Program and since have <b>developed websites</b> and <b>applications</b> using <b>HTML</b>, <b>CSS</b>, <b>SCSS</b>, <b>Javascript</b>, <b>React</b>, and <b>Python</b>
+              </p> 
+            </span>
+            </div> 
+              <h3> Currently Seeking Full Employment Opportunities!</h3>
+            <div class="button">
+              <form action="#contact">
+                <button>Get in touch!</button>
+              </form>
+            </div>
+          </div>
+  
+    
+
+    
         <div className="work" id ="work">
         <h1>Work Goes Here</h1>
       </div>
