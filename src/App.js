548716446089a2b9371acd6e7ca74ada609725a5
Bobby <Bobby@Bobbys-Creation-Station.local>
2019-01-07 12:44:29 -0700

    Added animations to about me section and formatted media queries

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Toolbar from './components/Toolbar/Toolbar';
 import SideDrawer from './components/SideDrawer/SideDrawer';
 import Backdrop from './components/Backdrop/Backdrop';
 import bp from './bp.JPG';
+import WhenInView from './components/About/About';
+import Bounce from 'react-reveal/Bounce';
 
 class App extends Component {
   state = {
@@ -37,22 +39,38 @@ class App extends Component {
           <div className = "portrait">
             <img alt="bp" src ={bp}></img>
           </div>
-          <h1> About Me </h1>
-             <h2>Hello! My name is Bobby Ping.  I am a Front End/React Developer and UI/UX Designer/Developer</h2>
-          <div className = "bio">
-            <span> 
-              <p>I am newer to Southern Utah and absolutely love it here! I love the outdoors and there is so much to see and do in this area. I am a father of two amazing children. We love to travel, hike, cook and try new foods, and play music. I also love art, arcitecture, and photography.After working for many years as an <b>IT Professional</b>, I <b>developed</b> a passion for <b>user experience</b>, <b>user interface</b>, and <b>development</b> in general.
-                I decided to get serious about <b>web development</b> and enrolled and graduated from Bottega Tech's Full Stack Certification Program and since have <b>developed websites</b> and <b>applications</b> using <b>HTML</b>, <b>CSS</b>, <b>SCSS</b>, <b>Javascript</b>, <b>React</b>, and <b>Python</b>
-              </p> 
-            </span>
-            </div> 
-              <h3> Currently Seeking Full Employment Opportunities!</h3>
-            <div class="button">
-              <form action="#contact">
-                <button>Get in touch!</button>
-              </form>
-            </div>
-          </div>
+          <WhenInView>
+            {({ isInView }) =>
+              <Bounce bottom hide={!isInView}>      
+                   <h1> About Me </h1>
+                   <h2>Hello! My name is Bobby Ping.  I am a Front End/React Developer and UI/UX Designer/Developer</h2>
+                <div className ="bio">
+                 <h3>
+                 I am newer to Southern Utah and absolutely love it here! I love the outdoors
+                 and there is so much to see and do in this area. I am a father of two amazing 
+                 children and couldn't be luckier. We love to travel, hike, cook and try new foods 
+                 and play music. I also love art, arcitecture, and photography. After working 
+                 for many years as an IT Professional, I developed a passion for user experience, 
+                 user interface, and development in general. I decided to get serious about web
+                 development and enrolled and graduated from Bottega Tech's Full Stack Certification
+                 Program and since have developed websites and applications using HTML, CSS, SCSS, 
+                 Javascript, React, and Python. There is NOTHING I would rather be doing as a career.
+                </h3> 
+                </div>
+                  
+              
+                    <h3> Currently Seeking Full Time Employment Opportunities!</h3>
+                <div class="button">
+                  <form action="#contact">
+                    <button>Get in touch!</button>
+                  </form>
+                </div>
+          
+                </Bounce>
+              }
+            </WhenInView>
+
+        </div>
   
     
 
@@ -66,7 +84,7 @@ class App extends Component {
     </video>-->*/}
         <h1>Contact Me Goes Here</h1>
         </div>
-      </div>
+    </div>
 
   
 
